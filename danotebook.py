7a862032cd0c8314c5831ffc5663d20dfd6f335a
PythonScriptPlusPlus <48883024+PythonScriptPlusPlus@users.noreply.github.com>
2019-04-14 11:06:18 +0300

    Update danotebook.py

--- danotebook.py
+++ danotebook.py
@@ -1,7 +1,25 @@
+from operator import attrgetter
 from tkinter import *
+from tkinter import messagebox
+
+class Info:
+    def __init__(self,NameW,DateW,PhoneW,DiscW):
+        self.NameW = NameW
+        self.DateW = DateW
+        self.PhoneW = PhoneW
+        self.DiscW = DiscW
+    
+
+    
+
+NameW = StringVar()
+DateW = StringVar()
+PhoneW = StringVar()
+DiscW = StringVar()
 
 read = 'test'
 tk = Tk()
+tk.title("Контактная книга")
 with open('list.txt','r') as f:
     read = f.read()
     if read == '1':
@@ -30,21 +48,22 @@ dis2.grid(row = 5, column = 2, columnspan = 2)
 lab = Label(List, text = read, bg = 'white', fg = 'black', height = 14, width = 17)
 pic = Label(tk,bg = 'green') 
 
-mod = Button(modF, text = 'mod', width = 20)
-delete = Button(delF, text = 'delete', width = 21)
-add = Button(addF, text = 'add', width = 17)
-
-Name = Entry(name, bg = 'grey', fg = 'black', width = 41)
+Name = Entry(name, bg = 'grey', fg = 'black', width = 41, textvariable = NameW)
 discName = Label(name, text = "Name, scnd name,\nsurname", width = 42)
 
-Date = Entry(date, bg = 'grey', fg = 'black', width = 20)
+Date = Entry(date, bg = 'grey', fg = 'black', width = 20, textvariable = DateW)
 discDate = Label(date, text = "date of birth", width = 21)
 
-Phone = Entry(phone, bg = 'grey', fg = 'black', width = 20)
+Phone = Entry(phone, bg = 'grey', fg = 'black', width = 20, textvariable = PhoneW)
 discPhone = Label(phone, text = "phone number", width = 21)
 
-Disc = Entry(dis2, bg = 'grey', fg = 'black', width = 41)
+Disc = Entry(dis2, bg = 'grey', fg = 'black', width = 41, textvariable = DiscW)
 discDisc = Label(dis1, text = "discription", width = 21)
+
+    
+mod = Button(modF, text = 'mod', width = 20)
+delete = Button(delF, text = 'delete', width = 21)
+add = Button(addF, text = 'add', width = 17, command = aDd)
 
 lab.pack()
 pic.grid(row = 2, column = 3, rowspan = 5)
